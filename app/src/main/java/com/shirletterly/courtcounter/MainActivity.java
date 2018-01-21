0d81467b074f05eaabf660e88d6f477cd363186c
Shirlette Chambers <shirlette.chambers@gmail.com>
2018-01-20 21:38:49 -0500

    added functionality to buttons to add the correct number to the score of team A

--- app/src/main/java/com/shirletterly/courtcounter/MainActivity.java
+++ app/src/main/java/com/shirletterly/courtcounter/MainActivity.java
@@ -2,8 +2,39 @@ package com.shirletterly.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+
+//    increase the score by three
+    public void addThreeForteamA(View v){
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    //    increase the score by two
+    public void addTwoForteamA(View v){
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    //    increase the score by one
+    public void addOneForteamA(View v){
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
